e4fd9fee8715ae9278a4c24e53ae711cccd59ab9
mwgravitt <mwgravitt@spscommerce.com>
2015-03-08 12:45:41 -0400

    Passed all rankhospital tests

--- rankhospital.R
+++ rankhospital.R
@@ -1,54 +1,41 @@
 
-subsetandsort <- function (inHospitals, inState, columnName) {
+# abstracted these basic operations that take place regardless of the condition
+# subset the data and sort 
+subsetandsort <- function (inHospitals, inState, inColumnNum) {
+  
+  # coerce the column to numeric - required to remove missing values
+  inHospitals[[inColumnNum]] <- suppressWarnings(as.numeric (inHospitals[[inColumnNum]]))
   
   # subset to just a single state and remove NA
-  inHospitals <- subset (inHospitals, !is.na(inHospitals[[columnName]]) & inHospitals$State == inState)
+  inHospitals <- subset (inHospitals, !is.na(inHospitals[inColumnNum]) & inHospitals$State == inState)
   
   # sort by the appropriate column, then by hospital name
-  sorted <- inHospitals[order(inHospitals[[columnName]], inHospitals$Hospital.Name),]
+  sorted <- inHospitals[order(inHospitals[inColumnNum], inHospitals$Hospital.Name),]
+  
   return (sorted)
 }
 
 rankhospital <- function(inState, condition, inRank = "best") {
   
   ## Read outcome data
-  outcome <-read.csv("outcome-of-care-measures.csv") #, colClasses="character")
+  outcome <-read.csv("outcome-of-care-measures.csv", colClasses="character")
   
   validCondition <- c("heart attack", "heart failure", "pneumonia")
   columnNum <- c(11,17,23)
   conditionsMetadata = data.frame(validCondition, columnNum)
   
-  outcome[columnNums[1]]
-  
-  names(outcome)[11] <- "heart.attack"
-  outcome$heart.attack <- suppressWarnings(as.numeric (outcome$heart.attack))
-  names(outcome)[17] <- "heart.failure"
-  outcome$heart.failure <- suppressWarnings(as.numeric (outcome$heart.failure))
-  names(outcome)[23] <- "pneumonia"
-  outcome$pneumonia <- suppressWarnings(as.numeric (outcome$pneumonia))
-  
-  ## Check that state and outcome are valid
-  validConditions = c('heart attack','heart failure','pneumonia')
-  if (!condition %in% validConditions) {
+  # Check that outcome is valid
+  if (!condition %in% conditionsMetadata$validCondition) {
     stop ("invalid outcome")  
   }
-  
-  validStates = unique (outcome$State)
-  if (!inState %in% validStates) {
+
+  # Check that state is valid
+  if (!inState %in% outcome$State) {
     stop ("invalid state")  
   }
   
-  if (condition == "heart attack") {
-    sorted <- subsetandsort(outcome, inState, "heart.attack")
-  }
-  else if (condition == "heart failure") {
-    hospitals <- subset (hospitals, !is.na(hospitals$heart.failure))
-    sorted <- hospitals[order(hospitals$heart.failure, hospitals$Hospital.Name),]
-  }
-  else {
-    hospitals <- subset (hospitals, !is.na(hospitals$pneumonia))
-    sorted <- hospitals[order(hospitals$pneumonia, hospitals$Hospital.Name),]
-  }
+  sorted <- subsetandsort (outcome, inState, 
+                           conditionsMetadata[conditionsMetadata$validCondition==condition,]$columnNum)
   
   if (inRank == "best") {
     inRank <- 1
